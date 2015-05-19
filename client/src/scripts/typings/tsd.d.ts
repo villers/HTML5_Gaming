8ed30701995915369e488454443e164dae6decb1
viller_m <mickael.villers@epitech.eu>
2015-05-19 10:20:33 +0200

    refactoring

--- client/src/scripts/typings/tsd.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../../bower_components/phaser-official/typescript/phaser.d.ts"/>
-
-/// <reference path="../Game.ts"/>
-/// <reference path="../state/Boot.ts"/>
-/// <reference path="../state/Preload.ts"/>
-/// <reference path="../state/Main.ts"/>
